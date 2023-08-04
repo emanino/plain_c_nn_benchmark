9e89edb1e4c91a8db8d5ae65205a48afd05ca0f4
Edoardo Manino <edoardo.manino@gmail.com>
2023-08-04 14:27:36 +0100

    [props] Added properties of polynomial approximation nets

---
+++ properties/poly_approx/poly_256_thresh_2_safe.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include "../keras2c/k2c_include.h"
+#include "../poly_nets/poly_256.h"
+
+int main()
+{
+	/* neural net input and output */
+    float input_array[1] = {0.0f}, output_array[1] = {0.0f};
+    k2c_tensor input_tensor = {&input_array[0],1,1,{1,1,1,1,1}};
+    k2c_tensor output_tensor = {&output_array[0],1,1,{1,1,1,1,1}};
+	
+	/* restrict the input around the location of maximum error (x=3) */
+	float x = nondet_float();
+	__ESBMC_assume(isgreaterequal(x, 2.9f) && islessequal(x, 3.0f));
+	
+	/* call the neural network with non-deterministic input */
+	input_array[0] = x;
+	poly_256(&input_tensor, &output_tensor);
+	
+	/* compare the network output with an affine approximation of the ground truth */
+	float y = 2.8873749999999974f * (x - 2.9f) - 0.16373749999999976f;
+	float diff = fabsf(y - output_array[0]);
+	
+	/* Try decreasing thresholds by a factor of two */
+	__ESBMC_assert(islessequal(diff, 0.14784259291726454294925294007324f), ""); /* Expected result: verification successful */
+	
+	return 0;
+}
---
+++ properties/poly_approx/poly_64_64_thresh_4_unsafe.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include "../keras2c/k2c_include.h"
+#include "../poly_nets/poly_16_16.h"
+
+int main()
+{
+	/* neural net input and output */
+    float input_array[1] = {0.0f}, output_array[1] = {0.0f};
+    k2c_tensor input_tensor = {&input_array[0],1,1,{1,1,1,1,1}};
+    k2c_tensor output_tensor = {&output_array[0],1,1,{1,1,1,1,1}};
+	
+	/* restrict the input around the location of maximum error (x=-2) */
+	float x = nondet_float();
+	__ESBMC_assume(isgreaterequal(x, -2.0f) && islessequal(x, -1.9f));
+	
+	/* call the neural network with non-deterministic input */
+	input_array[0] = x;
+	poly_16_16(&input_tensor, &output_tensor);
+	
+	/* compare the network output with an affine approximation of the ground truth */
+	float y = -2.637375000000004f * (x + 2.0f) - 0.5f;
+	float diff = fabsf(y - output_array[0]);
+	
+	/* Try decreasing thresholds by a factor of two */
+	__ESBMC_assert(islessequal(diff, 0.01341553037305318391563068959671f), ""); /* Expected result: verification failure */
+	
+	return 0;
+}
