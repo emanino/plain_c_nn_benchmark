489d7405a1795aec189ab2a7c6976d94ee4f7e81
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2023-08-10 11:34:48 +0100

    added cmake as a build generator

---
+++ includes/verifier_stubs.c
@@ -0,0 +1,9 @@
+#include <verifier_functions.h>
+
+// Just for the compiler to pass
+
+float __VERIFIER_nondet_float() { return 0; }
+void __VERIFIER_assume(int cond) {}
+void reach_error() {}
+
+void __VERIFIER_assert(int cond) {}
