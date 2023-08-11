cefcd1e7d38b65bf1e9167cb3e7fbfeb109f709e
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-08-11 18:52:23 +0100

    clean.sh now remove malloc attributes (from gcc)

--- benchexec-extras/clean.sh
+++ benchexec-extras/clean.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # Find every .i in folder and then remove unsupported features
+# TODO: we could definetely use a loop or combine all the seds into one.
 
 # No _Float128
 find $1 -iname *.i | xargs sed -i -e '/_Float128/,+1d'
+
+# No gcc malloc stuff (clang does not support this)
+find $1 -iname *.i | xargs sed -i -e 's/__attribute__ ((__malloc__)))/ /g'
+find $1 -iname *.i | xargs sed -i -e 's/__attribute__ ((__malloc__ (__builtin_free, 1)))/ /g'
+find $1 -iname *.i | xargs sed -i -e 's/__attribute__ ((__malloc__ (reallocarray, 1)))/ /g'
+find $1 -iname *.i | xargs sed -i -e 's/__attribute__ ((__malloc__ (fclose, 1)))/ /g'
+find $1 -iname *.i | xargs sed -i -e 's/__attribute__ ((__malloc__ (pclose, 1)))/ /g'
