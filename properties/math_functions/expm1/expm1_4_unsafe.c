eef2b44c50db1fb9e0b3f8cac2c19cae414f6eb3
Edoardo Manino <edoardo.manino@gmail.com>
2024-05-29 11:45:17 +0100

    Added expm1f and log1pf to math_functions

---
+++ properties/math_functions/expm1/expm1_4_unsafe.c
@@ -0,0 +1,23 @@
+#include <verifier_functions.h>
+
+#include <math.h>
+
+#define EXP_CHECK_NEXT 1e-2f
+#define EXP_CHECK_RANGE 88.0f
+
+int main() /* check_derivative */
+{
+	float x1 = __VERIFIER_nondet_float();
+	
+	__VERIFIER_assume(isgreaterequal(x1, -EXP_CHECK_RANGE) && islessequal(x1, EXP_CHECK_RANGE)); /* Choose a range where precision is high */
+	
+	float x2 = x1 + EXP_CHECK_NEXT;
+	
+	float y1 = expm1f(x1);
+	float y2 = expm1f(x2);
+	float derivative = (y2 - y1) / EXP_CHECK_NEXT;
+	
+	__VERIFIER_assert(isgreaterequal(derivative, y2 + 1.0f)); /* Expected result: verification failure */
+
+    return 0;
+}
---
+++ properties/math_functions/log1p/log1p_4_safe.c
@@ -0,0 +1,25 @@
+#include <verifier_functions.h>
+
+#include <math.h>
+
+#define LOG_CHECK_NEXT 1e-2f
+#define LOG_CHECK_DIFF 1e-3f
+
+int main() /* check_derivative */
+{
+	float x1 = __VERIFIER_nondet_float();
+	float x2 = x1 + LOG_CHECK_NEXT;
+	
+	__VERIFIER_assume(isgreaterequal(x1, -1.0f) && !isinf(x1));
+	
+	float y1 = log1pf(x1);
+	float y2 = log1pf(x2);
+	float derivative = (y2 - y1) / LOG_CHECK_NEXT;
+	
+	float d2 = 1.0f / (1.0f + x2); /* analytical derivative of log(1 + x) at x = x2 */
+	float diff = derivative - d2;
+	
+	__VERIFIER_assert(isgreaterequal(diff, -LOG_CHECK_DIFF)); /* Expected result: verification successful */
+
+    return 0;
+}
