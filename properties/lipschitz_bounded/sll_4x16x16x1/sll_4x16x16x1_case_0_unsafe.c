55557ff475a22bce7dc00f25fb01c5e777e63d08
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-05 14:07:09 +0100

    Added lipschitz_bounded safety properties

---
+++ properties/lipschitz_bounded/sll_4x16x16x1/sll_4x16x16x1_case_0_unsafe.c
@@ -0,0 +1,27 @@
+#include <verifier_functions.h>
+
+#include <math.h>
+
+void entry(const float tensor_onnx__Pad_0[1][4], float tensor_43[1][1]);
+
+int main()
+{
+    float tensor_onnx__Pad_0[1][4];
+    float tensor_43[1][1];
+    
+	tensor_onnx__Pad_0[0][0] = __VERIFIER_nondet_float();
+	tensor_onnx__Pad_0[0][1] = __VERIFIER_nondet_float();
+	tensor_onnx__Pad_0[0][2] = __VERIFIER_nondet_float();
+	tensor_onnx__Pad_0[0][3] = __VERIFIER_nondet_float();
+	
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][0] >= -1.6f && tensor_onnx__Pad_0[0][0] <= -1.4f);
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][1] >= -1.6f && tensor_onnx__Pad_0[0][1] <= -1.4f);
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][2] >= -1.6f && tensor_onnx__Pad_0[0][2] <= -1.4f);
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][3] >= -1.6f && tensor_onnx__Pad_0[0][3] <= -1.4f);
+	
+	entry(tensor_onnx__Pad_0, tensor_43);
+	
+	__VERIFIER_assert(tensor_43[0][0] <= 0.390443f); /* test: 0.438916, bound: 0.574285 */
+
+    return 0;
+}
