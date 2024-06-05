ca7ebaa163b1a1fb608b5f002b39176544629f9d
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-05 14:07:09 +0100

    Added lipschitz_bounded safety properties

---
+++ properties/lipschitz_bounded/sll_3x12x12x1/sll_3x12x12x1_case_0_unsafe.c
@@ -0,0 +1,25 @@
+#include <verifier_functions.h>
+
+#include <math.h>
+
+void entry(const float tensor_onnx__Pad_0[1][3], float tensor_43[1][1]);
+
+int main()
+{
+    float tensor_onnx__Pad_0[1][3];
+    float tensor_43[1][1];
+    
+	tensor_onnx__Pad_0[0][0] = __VERIFIER_nondet_float();
+	tensor_onnx__Pad_0[0][1] = __VERIFIER_nondet_float();
+	tensor_onnx__Pad_0[0][2] = __VERIFIER_nondet_float();
+	
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][0] >= -1.6f && tensor_onnx__Pad_0[0][0] <= -1.4f);
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][1] >= -1.6f && tensor_onnx__Pad_0[0][1] <= -1.4f);
+	__VERIFIER_assume(tensor_onnx__Pad_0[0][2] >= -1.6f && tensor_onnx__Pad_0[0][2] <= -1.4f);
+	
+	entry(tensor_onnx__Pad_0, tensor_43);
+	
+	__VERIFIER_assert(tensor_43[0][0] <= 0.597381f); /* test: 0.639886, bound: 0.756417 */
+
+    return 0;
+}
