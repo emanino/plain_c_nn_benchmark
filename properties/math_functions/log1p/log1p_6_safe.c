34f8380215f79d7c3ee518c983a1f393b3faec07
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-12 19:55:21 +0100

    Threshold calibration of log1p properties

--- properties/math_functions/log1p/log1p_6_safe.c
+++ properties/math_functions/log1p/log1p_6_safe.c
@@ -2,19 +2,19 @@
 
 #include <math.h>
 
-#define LOG_CHECK_RANGE 3.4e38f
-#define LOG_CHECK_ERROR 1e-4f
+#define LOG1P_CHECK_RANGE 3.4e38f
+#define LOG1P_CHECK_ERROR 1e-4f
 
 int main() /* check_inverse */
 {
 	float x = __VERIFIER_nondet_float();
 	
-	__VERIFIER_assume(isgreaterequal(x, -1.0f) && islessequal(x, LOG_CHECK_RANGE));
+	__VERIFIER_assume(isgreaterequal(x, -1.0f) && islessequal(x, LOG1P_CHECK_RANGE));
 	
 	float y = expm1f(log1pf(x));
 	float r = fabsf(x - y) / (fabsf(x) + 1e-37f); /* relative error: avoid division by zero */
 	
-	__VERIFIER_assert(islessequal(r, LOG_CHECK_ERROR)); /* Expected result: verification successful */
+	__VERIFIER_assert(islessequal(r, LOG1P_CHECK_ERROR)); /* Expected result: verification successful */
 
     return 0;
 }
