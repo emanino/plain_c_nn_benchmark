d60a99c3733adadf5e382893b6cd9b96a8d927e9
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-13 22:35:26 +0100

    fixed linear properties

--- properties/neural_layers/linear/linear_9_unsafe.c
+++ properties/neural_layers/linear/linear_9_unsafe.c
@@ -2,7 +2,7 @@
 
 #include <math.h>
 
-#define INPUT_SIZE 2
+#define INPUT_SIZE 4
 #define OUTPUT_SIZE 2
 
 /* simplified linear layer */
@@ -19,24 +19,28 @@ void linear(const float* x, float* y, const float W[OUTPUT_SIZE][INPUT_SIZE], co
 
 #define LINEAR_CHECK_RANGE 1e37f
 
-int main() /* check_rectangular_size_2 */
+int main() /* check_rectangular_size_4 */
 {
 	float x[INPUT_SIZE], y[OUTPUT_SIZE];
 	
-	float W[OUTPUT_SIZE][INPUT_SIZE] = {{ 2.3f, -2.3f},
-										{-2.3f,  2.3f}};
+	float W[OUTPUT_SIZE][INPUT_SIZE] = {{ 2.3f,  2.3f, -2.3f, -2.3f},
+										{-2.3f, -2.3f,  2.3f,  2.3f}};
 	
 	float b[OUTPUT_SIZE] = {0.0f};
 	
 	x[0] = __VERIFIER_nondet_float();
 	x[1] = __VERIFIER_nondet_float();
+	x[2] = __VERIFIER_nondet_float();
+	x[3] = __VERIFIER_nondet_float();
 	
 	__VERIFIER_assume(isgreater(x[0], LINEAR_CHECK_RANGE) && isless(x[0], LINEAR_CHECK_RANGE));
 	__VERIFIER_assume(isgreater(x[1], LINEAR_CHECK_RANGE) && isless(x[1], LINEAR_CHECK_RANGE));
+	__VERIFIER_assume(isgreater(x[2], LINEAR_CHECK_RANGE) && isless(x[2], LINEAR_CHECK_RANGE));
+	__VERIFIER_assume(isgreater(x[3], LINEAR_CHECK_RANGE) && isless(x[3], LINEAR_CHECK_RANGE));
 	
 	linear(x, y, W, b, INPUT_SIZE, OUTPUT_SIZE);
 	
-	__VERIFIER_assert(y[0] == -y[1]); /* Expected result: verification successful */
+	__VERIFIER_assert(y[0] == -y[1]); /* Expected result: verification failure */
 
     return 0;
 }
