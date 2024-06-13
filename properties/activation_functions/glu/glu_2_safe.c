2ee17c83b630acfb664355bf293f01c7acaea13e
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-13 13:46:01 +0100

    Fixed glu properties

--- properties/activation_functions/glu/glu_2_safe.c
+++ properties/activation_functions/glu/glu_2_safe.c
@@ -12,6 +12,7 @@ float glu(float x, float y)
 	return x * logistic(y);
 }
 
+#define GLU_CHECK_X_RANGE 3.4e38
 #define GLU_CHECK_NEXT 1e-5f
 
 int main() /* check_non_decreasing_in_x */
@@ -20,7 +21,7 @@ int main() /* check_non_decreasing_in_x */
 	float x2 = x1 + GLU_CHECK_NEXT;
 	float y = __VERIFIER_nondet_float();
 	
-	__VERIFIER_assume(!isnan(x1) && !isnan(y));
+	__VERIFIER_assume(isgreaterequal(x1, -GLU_CHECK_X_RANGE) && islessequal(x1, GLU_CHECK_X_RANGE) && !isnan(y));
 	
 	float z1 = glu(x1, y);
 	float z2 = glu(x2, y);
--- properties/activation_functions/glu/glu_4_safe.c
+++ properties/activation_functions/glu/glu_4_safe.c
@@ -12,15 +12,17 @@ float glu(float x, float y)
 	return x * logistic(y);
 }
 
+#define GLU_CHECK_X_RANGE 3.4e38
+
 int main() /* check_symmetry_in_x */
 {
 	float x = __VERIFIER_nondet_float();
 	float y = __VERIFIER_nondet_float();
 	
-	__VERIFIER_assume(!isnan(x) && !isnan(y));
+	__VERIFIER_assume(isgreaterequal(x, -GLU_CHECK_X_RANGE) && islessequal(x, GLU_CHECK_X_RANGE) && !isnan(y));
 	
 	float z = glu(x, y);
-	float z_inv = glu(-x, y);
+	float z_inv = -glu(-x, y);
 	
 	__VERIFIER_assert(z == z_inv); /* Expected result: verification successful */
 
