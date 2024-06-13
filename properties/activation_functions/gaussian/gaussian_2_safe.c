f7f694127b29a1aafeceeca1c82a4abdc0e52c76
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-13 12:59:11 +0100

    Fixed gaussian finite difference properties

--- properties/activation_functions/gaussian/gaussian_2_safe.c
+++ properties/activation_functions/gaussian/gaussian_2_safe.c
@@ -7,24 +7,25 @@ float gauss(float x)
 	return expf(-x * x / 2.0f);
 }
 
-#define GAUSSIAN_CHECK_NEXT 1e-1f
-#define GAUSSIAN_CHECK_DIFF 1e-3f
+#define GAUSSIAN_CHECK_RANGE 1e38
+#define GAUSSIAN_CHECK_NEXT 1e-3f
+#define GAUSSIAN_CHECK_DIFF 1e-2f
 
 int main() /* check_derivative */
 {
 	float x1 = __VERIFIER_nondet_float();
 	float x2 = x1 + GAUSSIAN_CHECK_NEXT;
 	
-	__VERIFIER_assume(!isnan(x1) && !isinf(x1));
+	__VERIFIER_assume(isgreaterequal(x1, -GAUSSIAN_CHECK_RANGE) && islessequal(x1, GAUSSIAN_CHECK_RANGE));
 	
 	float y1 = gauss(x1);
 	float y2 = gauss(x2);
 	float derivative = (y2 - y1) / GAUSSIAN_CHECK_NEXT;
 	
-	float d = -x1 * gauss(x1); /* analytic derivative at x1 */
+	float d = -0.5 * (x1 + x2) * gauss(0.5 * (x1 + x2)); /* analytic derivative at avg(x1, x2) */
 	float diff = fabsf(derivative - d);
 	
-	__VERIFIER_assert(isgreaterequal(diff, GAUSSIAN_CHECK_DIFF)); /* Expected result: verification successful */
+	__VERIFIER_assert(islessequal(diff, GAUSSIAN_CHECK_DIFF)); /* Expected result: verification successful */
 
     return 0;
 }
