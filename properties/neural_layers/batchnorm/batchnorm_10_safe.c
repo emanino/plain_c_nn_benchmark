85b0985c8a073ec656768bca40ffcb9d603b140b
Edoardo Manino <edoardo.manino@gmail.com>
2024-06-13 17:13:32 +0100

    fixed batchnorm properties

--- properties/neural_layers/batchnorm/batchnorm_10_safe.c
+++ properties/neural_layers/batchnorm/batchnorm_10_safe.c
@@ -2,11 +2,15 @@
 
 #include <math.h>
 
+#define INPUT_SIZE 4
+#define BATCH_SIZE 2
+
 /* simplified batch normalisation */
-void batchnorm(const float* x, float* y, float* mean, float* std, const float* gamma, const float* beta, int n, int batch_size)
+void batchnorm(const float x[INPUT_SIZE][BATCH_SIZE], float y[INPUT_SIZE][BATCH_SIZE],
+               float* mean, float* std, const float* gamma, const float* beta, int n, int batch_size)
 {
-	int i, j, k;
-	
+	int i, j;
+
 	/* estimate batch mean */
 	for(i = 0; i < n; ++i) {
 		mean[i] = 0.0f;
@@ -14,7 +18,7 @@ void batchnorm(const float* x, float* y, float* mean, float* std, const float* g
 			mean[i] += x[i][j];
 		mean[i] /= (float) batch_size;
 	}
-	
+
 	/* estimate batch std */
 	for(i = 0; i < n; ++i) {
 		std[i] = 0.0f;
@@ -22,18 +26,16 @@ void batchnorm(const float* x, float* y, float* mean, float* std, const float* g
 			float diff = x[i][j] - mean[i];
 			std[i] += diff * diff;
 		}
-		std[i] = sqrtf(std[i] / ((float) batch_size - 1.0f)); /* corrected estimator */
+		std[i] = sqrtf(std[i] / ((float) batch_size - 1.0f)) + 1e-8; /* corrected estimator */
 	}
-	
+
 	/* apply normalisation */
 	for(i = 0; i < n; ++i)
 		for(j = 0; j < batch_size; ++j)
 			y[i][j] = gamma[i] * (x[i][j] - mean[i]) / std[i] + beta[i];
 }
 
-#define INPUT_SIZE 4
-#define BATCH_SIZE 2
-
+#define BATCHNORM_CHECK_RANGE 1e18
 #define BATCHNORM_CHECK_NEXT 1e-5f
 
 int main() /* check_non_decreasing_size_4_2 */
@@ -44,18 +46,18 @@ int main() /* check_non_decreasing_size_4_2 */
 	float beta[INPUT_SIZE] = {-0.42f, 0.33f, -0.12f, 0.01f};
 
 	x[0][0] = __VERIFIER_nondet_float();
-	x[0][1] = x[0][0] + MAXPOOL_CHECK_NEXT;
+	x[0][1] = x[0][0] + BATCHNORM_CHECK_NEXT;
 	x[1][0] = __VERIFIER_nondet_float();
-	x[1][1] = x[1][0] + MAXPOOL_CHECK_NEXT;
+	x[1][1] = x[1][0] + BATCHNORM_CHECK_NEXT;
 	x[2][0] = __VERIFIER_nondet_float();
-	x[2][1] = x[2][0] + MAXPOOL_CHECK_NEXT;
+	x[2][1] = x[2][0] + BATCHNORM_CHECK_NEXT;
 	x[3][0] = __VERIFIER_nondet_float();
-	x[3][1] = x[3][0] + MAXPOOL_CHECK_NEXT;
+	x[3][1] = x[3][0] + BATCHNORM_CHECK_NEXT;
 	
-	__VERIFIER_assume(!isnan(x[0][0]) && !isinf(x[0][0]));
-	__VERIFIER_assume(!isnan(x[1][0]) && !isinf(x[1][0]));
-	__VERIFIER_assume(!isnan(x[2][0]) && !isinf(x[2][0]));
-	__VERIFIER_assume(!isnan(x[3][0]) && !isinf(x[3][0]));
+	__VERIFIER_assume(isgreater(x[0][0], -BATCHNORM_CHECK_RANGE) && isless(x[0][0], BATCHNORM_CHECK_RANGE));
+	__VERIFIER_assume(isgreater(x[1][0], -BATCHNORM_CHECK_RANGE) && isless(x[1][0], BATCHNORM_CHECK_RANGE));
+	__VERIFIER_assume(isgreater(x[2][0], -BATCHNORM_CHECK_RANGE) && isless(x[2][0], BATCHNORM_CHECK_RANGE));
+	__VERIFIER_assume(isgreater(x[3][0], -BATCHNORM_CHECK_RANGE) && isless(x[3][0], BATCHNORM_CHECK_RANGE));
 	
 	batchnorm(x, y, mean, std, gamma, beta, INPUT_SIZE, BATCH_SIZE);
 	
--- properties/neural_layers/batchnorm/batchnorm_1_unsafe.c
+++ properties/neural_layers/batchnorm/batchnorm_1_unsafe.c
@@ -2,11 +2,15 @@
 
 #include <math.h>
 
+#define INPUT_SIZE 2
+#define BATCH_SIZE 2
+
 /* simplified batch normalisation */
-void batchnorm(const float* x, float* y, float* mean, float* std, const float* gamma, const float* beta, int n, int batch_size)
+void batchnorm(const float x[INPUT_SIZE][BATCH_SIZE], float y[INPUT_SIZE][BATCH_SIZE],
+               float* mean, float* std, const float* gamma, const float* beta, int n, int batch_size)
 {
-	int i, j, k;
-	
+	int i, j;
+
 	/* estimate batch mean */
 	for(i = 0; i < n; ++i) {
 		mean[i] = 0.0f;
@@ -14,7 +18,7 @@ void batchnorm(const float* x, float* y, float* mean, float* std, const float* g
 			mean[i] += x[i][j];
 		mean[i] /= (float) batch_size;
 	}
-	
+
 	/* estimate batch std */
 	for(i = 0; i < n; ++i) {
 		std[i] = 0.0f;
@@ -22,32 +26,31 @@ void batchnorm(const float* x, float* y, float* mean, float* std, const float* g
 			float diff = x[i][j] - mean[i];
 			std[i] += diff * diff;
 		}
-		std[i] = sqrtf(std[i] / ((float) batch_size - 1.0f)); /* corrected estimator */
+		std[i] = sqrtf(std[i] / ((float) batch_size - 1.0f)) + 1e-8; /* corrected estimator */
 	}
-	
+
 	/* apply normalisation */
 	for(i = 0; i < n; ++i)
 		for(j = 0; j < batch_size; ++j)
 			y[i][j] = gamma[i] * (x[i][j] - mean[i]) / std[i] + beta[i];
 }
 
-#define INPUT_SIZE 2
-#define BATCH_SIZE 2
+#define BATCHNORM_CHECK_RANGE 1e18
 
 int main() /* check_bound_size_2_2 */
 {
 	float x[INPUT_SIZE][BATCH_SIZE], y[INPUT_SIZE][BATCH_SIZE], mean[INPUT_SIZE], std[INPUT_SIZE];
 	
 	float gamma[INPUT_SIZE] = {0.123456f, 0.654321f};
-	float beta[INPUT_SIZE] = {-0.42f, 0.333333f};
+	float beta[INPUT_SIZE] = {-0.42f, -0.333333f};
 
 	x[0][0] = __VERIFIER_nondet_float();
 	x[0][1] = __VERIFIER_nondet_float();
 	x[1][0] = x[0][0];
 	x[1][1] = x[0][1];
 	
-	__VERIFIER_assume(isgreaterequal(x[0][0], 0.0f));
-	__VERIFIER_assume(isgreaterequal(x[0][1], 0.0f));
+	__VERIFIER_assume(isgreaterequal(x[0][0], 0.0f) && islessequal(x[0][0], BATCHNORM_CHECK_RANGE));
+	__VERIFIER_assume(isgreaterequal(x[0][1], 0.0f) && islessequal(x[0][1], BATCHNORM_CHECK_RANGE));
 	
 	batchnorm(x, y, mean, std, gamma, beta, INPUT_SIZE, BATCH_SIZE);
 	
