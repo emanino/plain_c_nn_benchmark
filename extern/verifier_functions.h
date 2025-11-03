77da497f4df0506292c810a90cf90e29b8d17fca
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-11-03 18:01:43 +0000

    added reach_error definition

--- extern/verifier_functions.h
+++ extern/verifier_functions.h
@@ -2,13 +2,14 @@
 #define _VERIFICATION_FUNCTIONS_H
 
 #include <stdlib.h> // for abort
+#include <assert.h> // for assert
 // Functions for verification (harness).
 // Mainly consists of assume, reach_error, and nondets
 // See: https://sv-comp.sosy-lab.org/2023/rules.php
 
 float __VERIFIER_nondet_float();
 #define __VERIFIER_assume(cond) if(!(cond)) abort();
-void reach_error();
+inline void reach_error() { assert(0); }
 
 #define __VERIFIER_assert(cond) if(!(cond)) reach_error()
 
